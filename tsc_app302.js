7518bef31d8e2946a27ba834f3228c7b64c25446
Thobif <jirayootpandee537@gmail.com>
2021-11-17 12:57:49 +0700

    Update V1

---
+++ tsc_app302.js
@@ -0,0 +1,4 @@
+var a = '1500'; //javascript Type
+var b = 'Welcome to TypeScript'; //TypeScript Type
+console.log(a);
+console.log(b);
---
+++ tsc_app302.ts
@@ -0,0 +1,4 @@
+let a: string= '1500' //javascript Type
+let b: string = 'Welcome to TypeScript' //TypeScript Type
+console.log(a)
+console.log(b)
